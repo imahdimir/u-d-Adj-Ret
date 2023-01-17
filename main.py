f4f528c827052a4d9ee889525375c87c33bc53a3
imahdimir <imahdimir@gmail.com>
2023-01-17 16:06:38 +0330

    v0.0.0

---
+++ main.py
@@ -0,0 +1,24 @@
+"""
+
+    """
+
+from pathlib import Path
+
+import cal_adj_returns
+import check_backward_compatibility
+
+def main() :
+    pass
+
+    ##
+    cal_adj_returns.main()
+
+    ##
+    check_backward_compatibility.main()
+
+##
+
+
+if __name__ == "__main__" :
+    main()
+    print(f'{Path(__file__).name} Done!')
